272343cf4ed11f08eab848327620b9eae80740ff
leowu0329 <ryowuandjanet@gmail.com>
2025-04-12 12:04:08 +0000

    #26.add pre production changes'

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -14,13 +15,22 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
   
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/user', userRoutes);
 app.use("/api/auth", authRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
